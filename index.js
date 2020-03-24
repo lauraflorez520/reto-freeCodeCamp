a1dc06510305e2b437492120dd11ec2e5097e7e4
Leina Pulgarin <leinatamayo@gmail.com>
2020-03-23 22:35:13 -0500

    agregando fotos a la section ganadores

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ function leinita() {
             hola[i] = numeros;
         }
 
-        return `<h4>leina con un puntaje de <span>${hola[0]}<span> </h4>
-        <h4>camila con un puntaje de ${hola[1]} </h4>
-        <h4>antonio con un puntaje de ${hola[2]}`;
+        return `<section><article class="participante__uno participantes"><img src="./img/imagen1.jpg" alt="" class="img"><h4 class="participantes">leina con un puntaje de <span>${hola[0]}<span> </h4></article>
+        <article class="participante__dos participantes"><img src="./img/imagen2.jpg" alt="" class="img"><h4 class="participantes">camila con un puntaje de ${hola[1]} </h4></article></section>
+        <section><article class="participante__tres participantes"><img src="./img/imagen3.jpg" alt="" class="img"><h4 class="participantes">antonio con un puntaje de ${hola[2]} </h4></article>
+        <article class="participante__cuatro participantes"><img src="./img/imagen4.jpg" alt="" class="img"><h4 class="participantes">Mario con un puntaje de <span>${hola[3]} </h4></article></section>`;
         // return `leina con un puntaje de <span>${hola[0]}<span>,
         // <br>
         // camila con un puntaje de ${hola[1]}
@@ -22,11 +23,11 @@ function leinita() {
     };
 
     return calcularArray([
-        [3, 4, 6, 1000],
+        [3, 4, 6, 5555555555556666666666666666666666666],
         [-1, -5, -6, -2],
-        [2, 5, 4, 200]
+        [2, 5, 4, 200],
+        [(2, 5, 4, 300)]
     ]);
-
 
     // console.log(
     //     calcularArray([
@@ -42,13 +43,13 @@ var mostrar = leinita();
 
 const modificar = document.querySelector(".resultado__ganadores");
 const boton = document.querySelector(".button");
+const parrafo = document.getElementById("parrafo");
 const presionar = () => {
     // document.write(mostrar);
     modificar.innerHTML = mostrar;
+    // parrafo.innerHTML = parrafoUno;
 };
 
-    document.write(leinita() + "holalasss");
-
-
-
- 
+boton.addEventListener("click", presionar);
+// parrafo.innerHTML = document.write(uno.splice(0, 1));
+// parrafoUno = uno.splice(0, 1);
